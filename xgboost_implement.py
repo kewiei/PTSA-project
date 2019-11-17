e309218e59c110f92b54c641246a41be0d6dd8b5
Tianmu Zhao <43070291+kewiei@users.noreply.github.com>
2019-11-17 14:29:32 -0500

    basic implementation

--- xgboost_implement.py
+++ xgboost_implement.py
@@ -2,9 +2,9 @@ import numpy as np
 import xgboost as xgb
 import pandas as pd
 import gc
+from sklearn.metrics import accuracy_score,r2_score
 import matplotlib.pyplot as plt
 import time
-
 
 starttime = time.time()
 # read in data
@@ -20,14 +20,11 @@ gc.collect()
 endtime = time.time()
 print("It takes {}s to load data".format(endtime-starttime))
 
-
-
 maskTrain=(mainFrame.index.get_level_values(1)>=str(2005)) & (mainFrame.index.get_level_values(1)<str(2006))
 
 maskTest=(mainFrame.index.get_level_values(1)>=str(2006)) & (mainFrame.index.get_level_values(1)<str(2007))
 
 x_train=np.array(features[maskTrain])
-# print(targets.columns)
 y_train=np.array(targets['ztargetMedian5'][maskTrain])
 x_train[np.isinf(x_train)]=100000000
 y_train=y_train*1
@@ -39,20 +36,18 @@ param = {'max_depth':2, 'eta':1, 'objective':'binary:logistic' }
 num_round = 2
 model = xgb.train(param, dtrain, num_round)
 
-model.fit(x_train,y_train)
+x_test=np.array(features[maskTest])
+y_test = np.array(targets['ztargetMedian5'][maskTest])
+x_test[np.isinf(x_test)]=100000000
+y_test=y_test*1
+y_test=y_test.astype(int)
 
-test=features[maskTest]
-
-test[np.isinf(test)]=100000000
 # pred1=model.predict_proba(test)
-dtest = xgb.DMatrix(test)
+dx_test = xgb.DMatrix(x_test)
 # make prediction
-pred1 = model.predict(dtest)
+pred1 = model.predict(dx_test)
+pred1 = np.array(pred1)
 
-pred1=pd.DataFrame(pred1)
-pred1.set_index(features[maskTest].index,inplace=True)
-pred1.to_csv('xgboost_pred1SmallModelTest' + '2006' + '.csv')
+accuracy = r2_score(y_test,pred1)
 gc.collect()
-
-
-
+print(accuracy)
